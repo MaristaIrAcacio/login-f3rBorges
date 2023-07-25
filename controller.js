e66eed6a0ad5567410c5ac3ef37394d129cdfc7c
Fernanda <borges.p@alunoescolassociais.org.br>
2023-07-25 12:02:31 -0300

    alteração no JavaScript

--- controller.js
+++ controller.js
@@ -9,3 +9,47 @@ function acessar() {
 		console.log(window.location.href);
     }
 }
+
+var dadosLista = [];
+
+function salvarUser()
+
+{
+    let nomeUser = document.getElementById("nomeUser").value;
+
+     if (nomeUser) {
+        dadosLista.push(nomeUser);
+        criarLista();
+        document.getElementById("nomeUser").value = '';
+    }
+
+}
+//INSERE NOME NA TABELA ABAIXO DO BOTÃO SALVAR
+function criarLista()
+{
+    let table = document.getElementById("tabela").innerHTML = "<tr><th>Nome Usuário</th><th>Ações</th></tr>"
+
+    for(let i=0; i <= (dadosLista.length-1); i++)
+
+    {
+
+        table += "<tr><td>" + dadosLista[i] + "</td><td><button class='btn btn-success' onclick='editar(this.parentNode.parentNode.rowIndex)'>Editar</button><button class='btn btn-danger' onlick=''>Excluir</button></td></tr>"; 
+        document.getElementById('tabela').innerHTML = table;
+
+    }
+
+}
+
+function editar(i)
+
+{
+    document.getElementById('nomeUser').value = dadosLista[(i - 1)];
+    dadosLista.splice(dadosLista[(i - 1), 1]);
+}
+// FUNÇÃO PARA EXCLUIR NOME
+
+function excluir(i){
+    dadosLista.splice((i-1),1);
+    document.getElementById("tabela").deleteRow(i);
+}
+
