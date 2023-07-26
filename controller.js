31059bd122b16568c356ca4745ee1d9ebf2e22bd
Fernanda <borges.p@alunoescolassociais.org.br>
2023-07-26 09:20:16 -0300

    alteração de JavaScript

--- controller.js
+++ controller.js
@@ -1,55 +1,46 @@
-function acessar() {
-    let loginEmail = document.getElementById("loginEmail").value;
-    let loginSenha = document.getElementById("loginSenha").value;
- 
-    if(!loginEmail || !loginSenha) {
-        alert("favor preencha os campos para acessar o site");
+// bloco de verificação de preenchimento de campos
+
+function Verifica(){
+    let user = document.getElementById('loginEmail').value;
+    let senha = document.getElementById ('liginSenha').value;
+    
+    if (luser || !senha){
+        alert("campos de preenchimento obrigatório. favor preencher! ");
     } else {
-        window.location.href = "aula2.html";
-		console.log(window.location.href);
+        alert("campos preenchidos com suesso!");
     }
 }
 
 var dadosLista = [];
 
-function salvarUser()
-
-{
-    let nomeUser = document.getElementById("nomeUser").value;
-
-     if (nomeUser) {
+function salvarUser() {
+    let nomeUser = document.getElementById("nomeUser").value;   
+    
+        if(nomeUser){
         dadosLista.push(nomeUser);
-        criarLista();
+        criaLista();
         document.getElementById("nomeUser").value = '';
-    }
-
-}
-//INSERE NOME NA TABELA ABAIXO DO BOTÃO SALVAR
-function criarLista()
-{
-    let table = document.getElementById("tabela").innerHTML = "<tr><th>Nome Usuário</th><th>Ações</th></tr>"
-
-    for(let i=0; i <= (dadosLista.length-1); i++)
-
-    {
-
-        table += "<tr><td>" + dadosLista[i] + "</td><td><button class='btn btn-success' onclick='editar(this.parentNode.parentNode.rowIndex)'>Editar</button><button class='btn btn-danger' onlick=''>Excluir</button></td></tr>"; 
-        document.getElementById('tabela').innerHTML = table;
-
-    }
-
+     }
 }
 
-function editar(i)
+function criaLista(){
+    let tabela = document.getElementById("tabela").innerHTML = "<tr><th>Nome usuario</th><th>Ações</th></tr>";
+    
+    for(let i = 0; i <= (dadosLista.length-1); i++){
+        tabela += "<tr><td>" + dadosLista[i] + "</td><td><button class='btn btn-success'onclick='editar (this.parentNode.parentNode.rowIndex)'>Editar</button><button class='btn btn-danger'onclick='excluir(this.parentNode.parentNode.rowIndex)'>Excluir</button></td></tr>";
+         document.getElementById('tabela').innerHTML = tabela;
+    };
+};
 
-{
-    document.getElementById('nomeUser').value = dadosLista[(i - 1)];
-    dadosLista.splice(dadosLista[(i - 1), 1]);
-}
-// FUNÇÃO PARA EXCLUIR NOME
+//Função para excluir nome
 
 function excluir(i){
-    dadosLista.splice((i-1),1);
-    document.getElementById("tabela").deleteRow(i);
+    dadosLista.splice((i-1), 1);
+
+document.getElementById("tabela").deleteRow(i);
 }
 
+function editar(i) {
+    document.getElementById("nomeUser").value = dadosLista[(i-1)];
+    dadosLista.splice(dadosLista[(i-1),1]);
+};
